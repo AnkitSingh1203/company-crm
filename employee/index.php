98ddff939f9bdf10b5ae3bc5b80c61000a8dd7c4
Ankit Singh <ankitstudent2003@gmail.com>
2026-07-09 22:06:45 +0530

    Add employee module

---
+++ employee/index.php
@@ -0,0 +1,32 @@
+<?php
+$page_title="Employees";
+include "../components/header.php";
+?>
+
+<div class="main-content">
+
+<?php include "../components/sidebar.php"; ?>
+
+<div class="content">
+
+<?php include "../components/navbar.php"; ?>
+
+<div class="page-header">
+
+<h1>Employees</h1>
+
+<button class="add-btn">
+
+<i class="fa-solid fa-plus"></i>
+
+Add Employee
+
+</button>
+
+</div>
+
+</div>
+
+</div>
+
+<?php include "../components/footer.php"; ?>
